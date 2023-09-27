393c461589f0a1ddbc8a07f093fdb8064a9b4053
Antonio Souza <antonios16020@gmail.com>
2023-09-27 18:28:45 -0300

    ïnitial commit

---
+++ threads-clone/Core/Authentication/Views/LoginView.swift
@@ -0,0 +1,22 @@
+//
+//  LoginView.swift
+//  threads-clone
+//
+//  Created by Antonio Garcia on 27/09/23.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Image("threads-app-icon")
+            }
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
