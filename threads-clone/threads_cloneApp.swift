29be98c170a5fdc3ee080eaf2654e4c767759c93
Antonio Souza <antonios16020@gmail.com>
2023-09-27 18:05:01 -0300

    Initial Commit

---
+++ threads-clone/threads_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  threads_cloneApp.swift
+//  threads-clone
+//
+//  Created by Antonio Garcia on 27/09/23.
+//
+
+import SwiftUI
+
+@main
+struct threads_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
