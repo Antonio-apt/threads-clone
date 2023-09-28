e6f68cdd222df49b929f1fb2359792b43b6948b6
Antonio Souza <antonios16020@gmail.com>
2023-09-27 23:38:20 -0300

    add feed

---
+++ threads-clone/Core/Feed/View/ThreadCell.swift
@@ -0,0 +1,79 @@
+//
+//  ThreadCell.swift
+//  threads-clone
+//
+//  Created by Antonio Garcia on 27/09/23.
+//
+
+import SwiftUI
+
+struct ThreadCell: View {
+    var body: some View {
+        VStack {
+            HStack(alignment: .top, spacing: 12) {
+                 Image("myself")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 40, height: 40)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                
+                VStack(alignment: .leading, spacing: 4) {
+                    HStack {
+                        Text("Antonio Souza")
+                            .font(.footnote)
+                        .fontWeight(.semibold)
+                        
+                        Spacer()
+                        
+                        Text("10m")
+                            .font(.caption)
+                            .foregroundColor(Color(.systemGray3))
+                        
+                        Button{
+                            
+                        } label: {
+                            Image(systemName: "ellipsis")
+                                .foregroundColor(Color(.darkGray))
+                        }
+                    
+                    }
+                    
+                    Text("Test Test Test")
+                        .font(.footnote)
+                        .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                    
+                    HStack(spacing: 16) {
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "heart")
+                        }
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "bubble.right")
+                        }
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "arrow.rectanglepath")
+                        }
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "paperplane")
+                        }
+                    }
+                    .foregroundColor(.black)
+                    .padding(.vertical, 8)
+                }
+            }
+            Divider()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ThreadCell()
+}
